320ac032b4e8aac548025c8d51c873941a176e17
minhduy1997 <32605236+minhduy1997@users.noreply.github.com>
2023-07-20 17:30:17 +0700

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Web.Http;
+using Newtonsoft.Json;
+using ToolsApp.App_Start;
+using ToolsApp.Authentication;
+using ToolsApp.Utilities;
+
+namespace ToolsApp
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            Mapper.RegisterMappings();
+        }
+        protected void Application_PostAuthenticateRequest(Object sender, EventArgs e)
+        {
+            #region Check BE
+            HttpCookie authCookie = Request.Cookies[FormsAuthentication.FormsCookieName + Parameters.CookieName.Cookie_Name];
+            if (authCookie != null)
+            {
+                if (authCookie.Value.Length > 0)
+                {
+                    FormsAuthenticationTicket authTicket = FormsAuthentication.Decrypt(authCookie.Value);
+
+                    try
+                    {
+                        CustomPrincipalSerializeModel serializeModel = JsonConvert.DeserializeObject<CustomPrincipalSerializeModel>(authTicket.UserData);
+                        if (serializeModel!=null)
+                        {
+                            CustomPrincipal newUser = new CustomPrincipal(serializeModel.UserName);
+                            newUser.UserId = serializeModel.UserId;
+                            newUser.UserName = serializeModel.UserName;
+                            newUser.FullName = serializeModel.FullName;
+                            newUser.Email = serializeModel.Email;
+                            HttpContext.Current.User = newUser;
+                        }                        
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+                }
+            }
+            #endregion            
+        }
+    }
+}
